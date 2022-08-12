953ec258d1495328b3fdc8d2aa1bdc23f3598548
bek2518 <106804857+bek2518@users.noreply.github.com>
2022-08-12 21:19:50 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,44 +11,40 @@
  */
 int _printf(const char *format, ...)
 {
-unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	va_list list;
+	int (*funct)(va_list);
+	int i = 0;
+	int len_printed = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	if (format == NULL || *format == '\0')
 	{
-		if (format[i] == '%')
+		return (-1);
+	}
+	va_start(list, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			_putchar(format[i]);
+			len_printed++;
+		}
+		else if (format[i + 1] == '%')
+		{
+			i++;
+			_putchar('%');
+			len_printed++;
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			funct = choose_func(format[i + 1]);
+			if (funct != NULL)
+			{
+				len_printed += funct(list);
+				i++;
+			}
+		}
+		i++;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	va_end(list);
+	return (len_printed);
 }
